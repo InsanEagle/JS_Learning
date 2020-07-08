82736ea61272be9c8496af6a8f62c1f36ab23d94
InsanEagle <insaneachiever@gmail.com>
2020-07-08 23:34:17 +0300

    User Interactions

---
+++ Part_2/Lesson_5/script.js
@@ -0,0 +1,16 @@
+// User Interactions
+
+// 1. console.log() - Console output
+console.log("Hello user!");
+
+// 2. alert() - Modal window with "OK" button
+alert("Interesting...");
+
+// 3. confirm() - Modal window with "OK" and "Cancel" buttons which mean "true" and "false"
+let ans = confirm("Did you understand this thing?");
+console.log(ans);
+
+// 4. prompt(title, [default]) - Modal window with input (always string)
+let whatever = prompt("Type your age", ""); // Always let default for IE
+console.log(whatever);
+console.log(typeof(whatever));
