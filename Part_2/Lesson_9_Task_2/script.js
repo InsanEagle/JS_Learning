bc65bfa3577e6e8a23aa6d5fd9e8218778006f07
InsanEagle <insaneachiever@gmail.com>
2020-07-11 00:56:46 +0300

    task 2

---
+++ Part_2/Lesson_9_Task_2/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Какое будет выведено значение: let x = 5; alert( x++ ); ?
+//5 +
+
+// Чему равно такое выражение: [ ] + false - null + true ?
+//? - (NaN)
+
+// Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
+//2 +
+
+// Чему равна сумма [ ] + 1 + 2?
+//NaN - (12)
+
+// Что выведет этот код: alert( "1"[0] )?
+//undefined - (1)
+
+// Чему равно 2 && 1 && null && 0 && undefined ?
+//false - (null)
+
+// Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+//Нет - (false)
+
+// Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
+///true - (3)
+
+// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+//Да - (false)
+
+// Что выведет этот код: alert( +"Infinity" ); ?
+//NaN - (Infinity)
+
+// Верно ли сравнение: "ёжик" > "яблоко"?
+//? - (true)
+
+// Чему равно 0 || "" || 2 || undefined || true || falsе ?
+//true - (2)
+
+//let x = 5; alert( x++ );
+// console.log([ ] + false - null + true);
+//let y = 1; let b = y = 2; alert(b);
+// console.log([ ] + 1 + 2);
+// alert( "1"[0] );
+// console.log(2 && 1 && null && 0 && undefined);
+// console.log(!!( x && y ) == (x && y));
+// alert( null || 2 && 3 || 4 );
+// let ab = [1, 2, 3]; let ba = [1, 2, 3]; console.log(ab == ba);
+// alert( +"Infinity" );
+// console.log("ёжик" > "яблоко");
+// console.log(0 || "" || 2 || undefined || true || false);
+
+
+
