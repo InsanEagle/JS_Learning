64a4fba0d30384edeaf78f23e0a8e9490c8e130b
InsanEagle <insaneachiever@gmail.com>
2020-10-12 16:43:41 +0300

    Task 11

---
+++ Part 3/Lesson_23_Task_11/Task 11_2/script.js
@@ -0,0 +1,6 @@
+'use strict';
+let age = document.getElementById('age');
+function showUser(surname, name) {
+  alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+}
+showUser('White', 'George');
